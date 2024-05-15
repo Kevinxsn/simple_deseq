a0155c01e1b71b8fb02b4cccde581ca15dcbbf56
Kevinxsn <kevinxsn@outlook.com>
2024-05-15 15:06:57 -0700

    simple_texi finished

--- readme.md
+++ readme.md
@@ -1,1 +1,9 @@
-Here is simple_deseq
+Here is simple_deseq & simple_teximport
+
+
+## simple_teximport
+simple_texi in simple_teximport takes two argument, the list of path that gene result has, and the list of column names that given to each of the dataframe. 
+It will create three dataframe, abundance, counts and length. 
+Can beaccssed by .dataframe, .abundance, .length
+
+
---
+++ simple_teximport.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import numpy as np
+
+
+class simple_texi:
+    def __init__(self, list_df_path, column_names):
+        """Initialize the attributes to describe a car."""
+        self.df = [pd.read_table(i) for i in list_df_path]
+        self.column_names = column_names
+        self.abundance = merge_dataframes_on_column(self.df, 'gene_id', [['TPM'] for _ in self.df], column_names)
+        self.counts = merge_dataframes_on_column(self.df, 'gene_id', [['expected_count'] for _ in self.df], column_names)
+        self.length = merge_dataframes_on_column(self.df, 'gene_id', [['effective_length'] for _ in self.df], column_names)
+        
+    def __str__(self):
+        abundance_info = str([self.abundance.shape, self.abundance.columns])
+        counts_info = str([self.counts.shape, self.counts.columns])
+        length_info = str([self.length.shape, self.length.columns])
+        ans = f'abundance information: {abundance_info} \n count information: {counts_info} \n length information: {length_info}'
+        return ans
+        
+
+
+
+def merge_dataframes_on_column(dfs, join_column, columns_to_keep, column_name):
+    """
+    Merge multiple DataFrames on a specified join column and keep only specified columns.
+
+    Args:
+    dfs (list of pd.DataFrame): List of DataFrames to merge.
+    join_column (str): Name of the column to join on.
+    columns_to_keep (list of list of str): List of lists, where each sublist contains the names of the columns
+                                           to keep from the corresponding DataFrame.
+
+    Returns:
+    pd.DataFrame: A DataFrame resulting from merging all input DataFrames on the join_column and 
+                  keeping only the specified columns from each.
+    """
+    # Ensure columns_to_keep is valid
+    if len(dfs) != len(columns_to_keep):
+        raise ValueError("Each DataFrame must have a corresponding list of columns to keep.")
+
+    # Reduce the dataframes to only the columns to keep and the join column
+    reduced_dfs = [
+        df[[join_column] + columns] for df, columns in zip(dfs, columns_to_keep)
+        if join_column in df.columns and all(col in df.columns for col in columns)
+    ]
+    
+    # Merge all dataframes on the join column
+    merged_df = reduced_dfs[0]
+    for df in reduced_dfs[1:]:
+        merged_df = pd.merge(merged_df, df, on=join_column, how='inner')
+    merged_df.columns = [merged_df.columns[0]] + column_name
+    
+    return merged_df
